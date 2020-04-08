aa3bac67825fbdd430be81cf9fa5b3aca5c96376
To Hai Nam <namthit98@Tos-MacBook-Pro.local>
2020-04-08 23:39:08 +0700

    update

--- src/app.js
+++ src/app.js
@@ -1,23 +1,59 @@
-const express = require('express');
-const mongoose = require('mongoose');
-const cors = require('cors');
-const messageController = require('./controllers/message.controller')
-
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
+const aws = require('aws-sdk');
+const messageController = require("./controllers/message.controller");
+const multer = require("multer");
+const { makeid, uploadFileToS3 } = require("./libs/helper");
+const { CORE } = require("./constants")
 const app = express();
+
+var storage = multer.memoryStorage();
+var upload = multer({ storage: storage });
 
 mongoose.connect(process.env.DB_URI, {
   useNewUrlParser: true
 });
 
+const s3 = new aws.S3({
+  accessKeyId: CORE.ACCESS_KEY_ID_S3,
+  secretAccessKey: CORE.SECRET_ACCESS_KEY_S3
+});
+
 app.use(express.json());
 app.use(cors());
 
-app.post('/messages', messageController.saveMessage)
+app.post("/messages", messageController.saveMessage);
 
-app.get('/ping', (req, res, next) => {
+app.post(
+  "/api/users/upload-avatar",
+  upload.single("avatar"),
+  async (req, res, next) => {
+    const extFile = req.file.originalname.split('.').pop()
+    const filename = [makeid(20), extFile].join('.')
+
+    const avatar = await uploadFileToS3(s3, {
+      Bucket: 'codingame',
+      Key: `users/avatar/${filename}`,
+      ACL: 'public-read',
+      ContentType: req.file.mimetype,
+      Body: req.file.buffer
+    });
+
+    res.status(200).json({
+      success: true,
+      results: {
+        imageUrl: avatar
+      },
+      message: "Upload file successfully!"
+    });
+  }
+);
+
+app.get("/api/ping", (req, res, next) => {
   res.status(200).jsonp({
     success: true,
-    results: [],
+    results: null,
     message: `I'm COMP1640-NODEJS`
   });
 });
