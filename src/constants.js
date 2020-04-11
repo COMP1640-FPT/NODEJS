d18b2f5e6f0aa4e72504e8b5cc5741baf2d81554
To Hai Nam <namthit98@MacBook-Pro.local>
2020-04-11 23:15:34 +0700

    update env

--- src/constants.js
+++ src/constants.js
@@ -5,7 +5,7 @@ module.exports = {
     SECRET_EXPIRES: process.env.SECRET_EXPIRES || "1h",
     SECRET: process.env.SECRET || "namehandsome130498",
     DB_URI: process.env.DB_URI || "mongodb://root:rootPassword@db:27017/COMP1640?authSource=admin",
-    ACCESS_KEY_ID_S3: process.env.ACCESS_KEY_ID_S3 || "AKIAVF6JF4IEEA4653NX",
-    SECRET_ACCESS_KEY_S3: process.env.SECRET_ACCESS_KEY_S3 || "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"
+    ACCESS_KEY_ID_S3: process.env.ACCESS_KEY_ID_S3 || "",
+    SECRET_ACCESS_KEY_S3: process.env.SECRET_ACCESS_KEY_S3 || ""
   }
 };
